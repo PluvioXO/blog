4e228e4fea92d47df370a481c701055f0a22729a
PluvioXO <75335290+PluvioXO@users.noreply.github.com>
2024-11-23 18:50:45 +0000

    Update blogData.jsx

--- src/pages/blog/blogData/blogData.jsx
+++ src/pages/blog/blogData/blogData.jsx
@@ -2,8 +2,7 @@ import one from './photos/0_S1KQ-2vjFuKGfg8C.png'
 import two from './photos/download (1).jpeg'
 import three from './photos/sgd-1.jpg'
 import four from './photos/what_is_attention_cover-1024x768.jpg'
-import five from './photos/images (2).png'
-import six from './photos/differentiation_scheme.png'
+import five from './photos/Lyapunov_exponents_of_the_Mandelbrot_set_(The_mini-Mandelbrot)_-_Matlab.png'
 
 export const blogData = [
     {
@@ -29,5 +28,11 @@ export const blogData = [
         image: four,
         date:'2024 February',
         tag: ['Data Science', 'Statistics'],
+    },
+    {
+        name:'Discrete Approximation of N-Dimension Phase Systems',
+        image: five,
+        date:'2024 November',
+        tag: ['Data Science', 'Statistics', 'Chaos Theory'],
     }
 ]
